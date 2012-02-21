bd3cf585f24dde84a1f29d12cfcc11ea404eb07c
Marco Yin <xmarco.info@gmail.com>
2012-02-22 00:07:04 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Meetups::Application.config.secret_token = '55f0df7d331d2c1670e6a1581ccf68d3f77eef3a2bb21fcabdcc616b5e4967a56072139eae045e46aa9df99163b7182b609fd541464ba03407410bcd1511a261'
